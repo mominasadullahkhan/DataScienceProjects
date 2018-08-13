4704f2dbc68170280ceaa8d7e45ea79486ecf206
mominasadullahkhan <mominasadullah@gmail.com>
2018-08-13 16:03:29 -0700

    Update update.py

--- MovieReviewClassifierWebApp/update.py
+++ MovieReviewClassifierWebApp/update.py
@@ -25,7 +25,3 @@ cur_dir = os.path.dirname(__file__)
 clf = pickle.load(open(os.path.join(cur_dir, 'pkl_objects','classifier.pkl'), 'rb'))
 db = os.path.join(cur_dir, 'reviews.sqlite')
 update_model(db_path=db, model=clf, batch_size=10000)
-# Uncomment the following lines if you are sure that
-# you want to update your classifier.pkl file
-# permanently.
-#pickle.dump(clf, open(os.path.join(cur_dir, #'pkl_objects', 'classifier.pkl'), 'wb'), protocol=4)
